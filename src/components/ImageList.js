53d773055ce2e8b5b674dfadd60176c4e7cc1920
Thanh Trung <thanhtrung20022001@gmail.com>
2021-03-12 01:01:46 +0700

    gird system

---
+++ src/components/ImageList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/ImageList.css';
+
+const ImageList = props => {
+
+    const images = props.images.map( (image) => {
+        return <img src={image.urls.regular} alt={image.description} key={image.id} />
+    });
+
+    return (
+        <div className="image-list">{images}</div>
+    );
+};
+
+export default ImageList;
